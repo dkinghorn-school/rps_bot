7c4b25234cdff3f105d4b72666b4aec7a716f3f5
Connor Cook <cojoco@users.noreply.github.com>
2017-03-31 10:34:40 -0600

    SVM bot now accepts the input we agreed on in our meeting

--- bot_driver.py
+++ bot_driver.py
@@ -1,52 +1,46 @@
 #!/usr/bin/env python
 
 import json
-import numpy as np
 
 from svm_bot import SVMBot
 
 
-def convert_throw(throw_str):
-    """Returns a one-hot representation of a throw"""
-    if throw_str == 'rock':
-        converted_throw = [1, 0, 0]
-    elif throw_str == 'paper':
-        converted_throw = [0, 1, 0]
-    elif throw_str == 'scissors':
-        converted_throw = [0, 0, 1]
-    else:
-        raise ValueError('Throw was not rock, paper, or scissors')
-    return converted_throw
-
-
-def one_hot(throws):
-    """Returns a one-hot representation of the data"""
-    one_hot_arr = []
-    for throw in throws:
-        one_hot_arr.extend(convert_throw(throw))
-    return one_hot_arr
-
-
 def main():
     """Get the data and predict what will come next"""
+    # Hyperparameter
     n = 5
+    # Get the data
     with open('data/connordata.json') as ifh:
         data = json.load(ifh)
-    X = []
-    y = []
-    for i in range(len(data['throws']['player1'])-n-1):
-        my_throws = data['throws']['player1'][i:i+n]
-        my_throws_one_hot = one_hot(my_throws)
-        next_other_throw = data['throws']['player2'][i+n]
-        X.append(my_throws_one_hot)
-        y.append(next_other_throw)
+    # Transform the data
+    opp_name = data['player2']
+    player_throws = data['throws']['player1']
+    opp_throws = data['throws']['player2']
+    instances = []
+    for i in range(len(player_throws)-n-1):
+        my_throws = player_throws[i:i+n]
+        other_throws = opp_throws[i:i+n]
+        next_other_throw = opp_throws[i+n]
+        instance = {'player_name': opp_name,
+                    'output': next_other_throw,
+                    'previous_throws': {
+                        'opponents_throws': other_throws,
+                        'my_throws': my_throws
+                    }}
+        instances.append(instance)
+    # Train the model
     bot = SVMBot()
-    bot.fit(X, y)
-    last_n_throws = data['throws']['player1'][-n:]
-    last_n_throws_one_hot = one_hot(last_n_throws)
-    next_val = bot.predict([last_n_throws_one_hot])
-    print(bot.classifier.classes_)
-    print(next_val)
+    bot.fit(instances)
+    i = len(player_throws)-n-1
+    my_throws = player_throws[i:i+n]
+    other_throws = opp_throws[i:i+n]
+    instance = {'player_name': opp_name,
+                'previous_throws': {
+                    'opponents_throws': other_throws,
+                    'my_throws': my_throws
+                }}
+    probs = bot.predict(instance)
+    print(probs)
 
 
 if __name__ == '__main__':
--- svm_bot.py
+++ svm_bot.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
 """Runs a basic rock-paper-scissors bot"""
 
+import numpy as np
 from sklearn import svm
+
+
+def convert_throw(throw_str):
+    """Returns a one-hot representation of a throw"""
+    if throw_str == 'rock':
+        converted_throw = [1, 0, 0]
+    elif throw_str == 'paper':
+        converted_throw = [0, 1, 0]
+    elif throw_str == 'scissors':
+        converted_throw = [0, 0, 1]
+    else:
+        raise ValueError('Throw was not rock, paper, or scissors')
+    return converted_throw
+
+
+def one_hot(throws):
+    """Returns a one-hot representation of the data"""
+    one_hot_arr = []
+    for throw in throws:
+        one_hot_arr.extend(convert_throw(throw[0]))
+        one_hot_arr.extend(convert_throw(throw[1]))
+    return one_hot_arr
 
 
 class SVMBot:
@@ -9,26 +32,57 @@ class SVMBot:
     def __init__(self):
         self.classifier = svm.SVC(probability=True,
                                   decision_function_shape='ovr')
+        self.players = []
 
-    def fit(self, X, y):
+    def fit(self, instances):
         """
         Fits the Bot's classifier
-        :param X: The input values (what was previously played by us,
-                  what was previously played by the other player, as far
-                  out as we want to use in predicting)
-        :type X: [[string, string, string, ..., string]]
-        :param y: The output value (what the other player played next)
-        :type y: [string]
+        :param instances: The instances to train on.
+        :type instances: [{'output': str,
+                           'player_name': str,
+                           'previous_throws': {'opponents_throws': [str, ..., str],
+                                               'my_throws: [str, ..., str]
+                                              }}]
         :return: Nothing
         """
+        X = []
+        y = []
+        for inst in instances:
+            if inst['player_name'] not in self.players:
+                self.players.append(inst['player_name'])
+        for inst in instances:
+            name_index = self.players.index(inst['player_name'])
+            name_arr = np.zeros((len(self.players), 1))
+            name_arr[name_index] = 1
+            prev_throws = inst['previous_throws']
+            throws = one_hot(list(zip(prev_throws['opponents_throws'],
+                                      prev_throws['my_throws'])))
+            throws.extend(name_arr)
+            X.append(throws)
+            y.append(inst['output'])
         self.classifier.fit(X, y)
 
-    def predict(self, x):
+    def predict(self, inst):
         """
         Predicts the probabilities of the next value the other player will play
-        :param x: The previous n throws
-        :type x: [string, string, string, ..., string]
+        :param inst: The instance
+        :type inst: {'player_name': str
+                         'previous_throws': {'opponents_throws': [str, ..., str],
+                                             'my_throws': [str, ..., str]
+                                            }}
         :return: The probabilities of the other player's next throw
-        :rtype: [float, float, float]
+        :rtype: { 'rock': float, 'paper': float, 'scissors': float }
         """
-        return self.classifier.predict_proba(x)
+        x = []
+        # Use a name_arr of all zeros if we don't know the player
+        name_arr = np.zeros((len(self.players), 1))
+        if inst['player_name'] in self.players:
+            name_index = self.players.index(inst['player_name'])
+            name_arr[name_index] = 1
+        prev_throws = inst['previous_throws']
+        throws = one_hot(list(zip(prev_throws['opponents_throws'],
+                                  prev_throws['my_throws'])))
+        throws.extend(name_arr)
+        x.append(throws)
+        probs = self.classifier.predict_proba(x)
+        return {'rock': probs[0][1], 'paper': probs[0][0], 'scissors': probs[0][2]}
