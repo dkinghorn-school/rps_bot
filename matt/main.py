6e6ddff897e2a33533a18e08b07b3f3021743c83
Matt Crowder <mcrowder65@gmail.com>
2017-04-03 20:11:51 -0600

    added matt

---
+++ matt/main.py
@@ -0,0 +1,11 @@
+from naive_bayes import naive_bayes
+from sklearn import datasets
+import numpy as np
+irisDataset = datasets.load_iris()
+nb = naive_bayes()
+nb.train(irisDataset)
+newData = [5.8, 2.7, 3.9, 1.2] # should be 1
+
+nb.predict(np.array(newData).reshape((1, -1)))
+
+
